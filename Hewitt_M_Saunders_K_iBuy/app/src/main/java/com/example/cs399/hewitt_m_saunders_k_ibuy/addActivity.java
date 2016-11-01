a147dbbbcd435a8db003a8b70f9e48afb3bd5d83
mitchellnau <mmh327@nau.edu>
2016-10-31 20:15:45 -0700

    add item activity

---
+++ Hewitt_M_Saunders_K_iBuy/app/src/main/java/com/example/cs399/hewitt_m_saunders_k_ibuy/addActivity.java
@@ -0,0 +1,13 @@
+package com.example.cs399.hewitt_m_saunders_k_ibuy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class addActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add);
+    }
+}
