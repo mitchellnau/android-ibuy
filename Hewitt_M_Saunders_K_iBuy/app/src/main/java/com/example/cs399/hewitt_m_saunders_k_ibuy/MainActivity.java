7e7150717b9b3fd309698fdd10f8f9267f81d469
スーツ芋 <Erekyth@yahoo.com>
2016-11-15 04:16:03 -0700

    Webview Java source

--- Hewitt_M_Saunders_K_iBuy/app/src/main/java/com/example/cs399/hewitt_m_saunders_k_ibuy/MainActivity.java
+++ Hewitt_M_Saunders_K_iBuy/app/src/main/java/com/example/cs399/hewitt_m_saunders_k_ibuy/MainActivity.java
@@ -2,12 +2,17 @@ package com.example.cs399.hewitt_m_saunders_k_ibuy;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.WebView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private WebView mWebView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mWebView = (WebView) findViewById(R.id.activity_main_webview);
     }
 }
